a0a5d09beb66799a22dacd30e6e1c438111d8753
Shawn Peery <shawn.w8465@gmail.com>
2023-10-01 15:57:50 -0600

    Adding tests

--- src/theme/Top-Nav-Bar/Top-Nav-Bar.test.tsx
+++ src/theme/Top-Nav-Bar/Top-Nav-Bar.test.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import TopNavBar from './Top-Nav-Bar';
+import { profile } from 'console';
 
 describe('<TopNavBar />', () => {
-  test('it should mount', () => {
+  it('should mount', () => {
     render(<TopNavBar />);
 
     const topNavBar = screen.getByTestId('TopNavBar');
-
     expect(topNavBar).toBeInTheDocument();
+  });
+
+  it('should show a home link with correct href', () => {
+    render(<TopNavBar />);
+
+    const homeLink = screen.getByRole('link');
+
+    expect(homeLink).toBeInTheDocument();
+    expect(homeLink).toHaveTextContent(/home/i);
+  });
+
+  it('should show a profile link with correct href', () => {
+    render(<TopNavBar />);
+
+    const profileLink = screen.getByRole('link');
+
+    expect(profileLink).toBeInTheDocument();
+    expect(profileLink).toHaveTextContent(/profile/i);
   });
 });
--- src/theme/Top-Nav-Bar/Top-Nav-Bar.tsx
+++ src/theme/Top-Nav-Bar/Top-Nav-Bar.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 import styles from './Top-Nav-Bar.module.scss';
+import { Link } from 'gatsby';
 
 interface TopNavBarProps {}
 
 const TopNavBar: FC<TopNavBarProps> = () => (
   <div className={styles.TopNavBar} data-testid="TopNavBar">
     TopNavBar Component
+    <Link to="/profile">Profile</Link>
+    {/* <button>Test</button> */}
   </div>
 );
 
