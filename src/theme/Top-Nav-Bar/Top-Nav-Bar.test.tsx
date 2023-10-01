8b6e6f99df9c815df12fa621569b0b0d556e09ba
Shawn Peery <shawn.w8465@gmail.com>
2023-09-30 18:25:48 -0600

    Updating Tests Correctly

--- src/theme/Top-Nav-Bar/Top-Nav-Bar.test.tsx
+++ src/theme/Top-Nav-Bar/Top-Nav-Bar.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import TopNavBar from './TopNavBar';
+import TopNavBar from './Top-Nav-Bar';
 
 describe('<TopNavBar />', () => {
   test('it should mount', () => {
     render(<TopNavBar />);
-    
+
     const topNavBar = screen.getByTestId('TopNavBar');
 
     expect(topNavBar).toBeInTheDocument();
