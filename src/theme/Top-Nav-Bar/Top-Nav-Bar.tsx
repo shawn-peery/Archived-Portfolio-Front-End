8d888b01899f80bb8beed92de63faaee4fcaf2d8
Shawn Peery <shawn.w8465@gmail.com>
2023-10-02 13:59:39 -0600

    Working
    - Going to see if I can get it working in a different way

--- src/theme/Top-Nav-Bar/Top-Nav-Bar.tsx
+++ src/theme/Top-Nav-Bar/Top-Nav-Bar.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
-import styles from './Top-Nav-Bar.module.scss';
 import { Link } from 'gatsby';
+
+import * as styles from './Top-Nav-Bar.module.scss';
 
 interface TopNavBarProps {}
 
 const TopNavBar: FC<TopNavBarProps> = () => (
-  <div className={styles.TopNavBar} data-testid="TopNavBar">
+  <div className={(styles as any).TopNavBar} data-testid="TopNavBar">
     TopNavBar Component
     <Link to="/profile">Profile</Link>
     {/* <button>Test</button> */}
